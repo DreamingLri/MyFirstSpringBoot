bebc011daed7a293025a5a9cabdd05010cdaca11
DreamingLri <3507415455@qq.com>
2023-10-21 14:14:48 +0800

    add vue2

--- vue/component/src/App.vue
+++ vue/component/src/App.vue
@@ -1,15 +1,17 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <Hello></Hello>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import Hello from "./components/Hello.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    Hello
+    HelloWorld
   }
 }
 </script>
