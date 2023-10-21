b31d066c6a89ce5fabd4f0207ddfaf2846139d6d
DreamingLri <3507415455@qq.com>
2023-10-21 15:10:07 +0800

    add element-plus

--- vue/myfirstvue/src/App.vue
+++ vue/myfirstvue/src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <Hello></Hello>
+  <Movie v-for="movie in movies" :key="movie.id" :title="movie.title" :rating="movie.rating"></Movie>
 </template>
 
 <script>
-import Hello from "./components/Hello.vue";
+
+
+import Movie from "@/components/Movie.vue";
 
 export default {
   name: 'App',
+  data:function (){
+    return{
+      movies:[
+        {id: 1, title:"111", rating: 11},
+        {id: 2, title:"222", rating: 22},
+        {id: 3, title:"333", rating: 33},
+      ]
+    }
+  },
   components: {
-    Hello
+    Movie
   }
 }
 </script>
